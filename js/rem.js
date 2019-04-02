6a2e44c0e1b48f318ab259da3e66d8234f2e205a
naruto001 <naruto001code@gmail.com>
2019-04-02 16:54:26 +0800

    add new

--- js/rem.js
+++ js/rem.js
@@ -1,5 +1,22 @@
-(function(){
-	var html = document.documentElement;
-	var wHtml = html.getBoundingClientRect().width;
-	html.style.fontSize = wHtml/15 + "px";
-})()
+// (function(){
+// 	var html = document.documentElement;
+// 	var wHtml = html.getBoundingClientRect().width;
+// 	html.style.fontSize = wHtml/15 + "px";
+// })()
+document.addEventListener('DOMContentLoaded',function(){
+    var resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize'
+    function resize() {
+        var html = document.documentElement;
+        if(html.clientWidth > 320){
+            if(html.clientWidth < 640){
+              html.style.fontSize = ~~(html.clientWidth / 20) + 'px'
+            }else{
+                html.style.fontSize = '32px'
+            }
+        }else{
+            html.style.fontSize = '16px'
+        }
+    };
+    resize()
+    window.addEventListener(resizeEvt, resize, false)
+})
